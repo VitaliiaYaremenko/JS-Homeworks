20e98ab77447f147bebf650044ddd1c4d5c181b1
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 20:15:51 +0100

    hw_14 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const userNumber = +prompt('Write your number: ');
+let divider = 0;
+
+if (userNumber <=1 ){
+    console.log(NaN);
+}else
+
+    for (let i = 2; i <= userNumber; i++) {
+
+        if (userNumber % i === 0) {
+            divider = i;
+            console.log('Дільник', i);
+            break;
+        }
+    }
