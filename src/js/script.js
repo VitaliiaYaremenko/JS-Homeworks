bc2177307b2a12ec8ca534f38ecddbc74ef0f9ac
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 20:27:07 +0100

    hw_15 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-'use strict';
+let unpairedSum = 0;
+// outerLoop:
+    for (let i = 1; i <= 20; i++ ) {
+    if ( i % 2 !== 0) {
+        unpairedSum += i;
+        // break outerLoop;
+    }
+}
+alert(unpairedSum);
