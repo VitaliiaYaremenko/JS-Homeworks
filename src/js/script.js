b4e42b6a389bd29b7b8d38c3e6c329a7c16a0a42
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 20:14:40 +0100

    hw_13 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    console.log(sum);
+}
+
+// ____________________________________
+
+const array = [1, 2, 3, 4, 5];
+let square = 0;
+
+for (let i = 0; i < array.length; i++) {
+    square = square + (Math.pow(array[i], 2));
+    console.log(square);
+}
+
+// for (let i in array) {
+//     array[i] = array[i]**2;
+//     console.log(array);
+// }
