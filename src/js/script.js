ed61e4babf07b6fbc63af7196901ea32d005c10b
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 19:59:29 +0100

    hw_3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const x= Number(prompt('Введіть число:','x'));
+
+const y= Number(prompt('Введіть число:','y'));
+
+console.log('x + y = ', x + y);
+console.log('x - y = ', x - y);
+console.log('x / y = ', x / y);
+console.log('x * y = ', x * y);
