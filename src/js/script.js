bba92b079f3a33ae19abe9beecd7307494d6d8f4
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 20:04:29 +0100

    hw_6 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const firstNumber = +prompt('Введіть перше число: ');
+
+const secondNumber= +prompt('Введіть друге число: ');
+
+const plus = firstNumber + secondNumber;
+const minus = firstNumber - secondNumber;
+const share = firstNumber / secondNumber;
+const multiply = firstNumber * secondNumber;
+
+
+if (firstNumber && secondNumber !== null) {
+    alert('plus = ' + plus + '\nminus = ' + minus + '\nshare = ' + share + '\nmultiply = ' + multiply);
+} else {
+    alert('Невірно вказане значення, пробуйте ще раз :)')
+}
