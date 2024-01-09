de6d8a984df16c34d51fa6315a90260bc469ac8a
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 20:08:24 +0100

    hw_9 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const userX = +prompt('Write your number: ');
+const userY = +prompt('Write your number: ');
+const userZ = +prompt('Write your number: ');
+
+alert((userX + userY + userZ) / 3);
+
+
+// const average = [userX, userY, userZ].reduce((a, b) => a + b, 0);
+// const sum = average / [userX, userY, userZ].length;
+// alert(sum);
