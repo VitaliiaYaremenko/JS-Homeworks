4bd8d07695a9d0291df7bcf52eeb348f706f7a4f
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 19:58:02 +0100

    hw_2 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let admin;
+let name;
+name = 'John';
+admin = name;
+
+alert(admin);
+
+
+const ourPlanetName = 'Земля';
+const keeperUserName = 'Наталка';
+
+
+
+const sum = '10' + 10;
+console.log(typeof sum);
