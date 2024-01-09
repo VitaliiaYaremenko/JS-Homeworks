e57a20cf2636e65463e9e87ed99d502e3c42d4c4
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 20:05:48 +0100

    hw_7 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const userName = prompt('Hi! What is your name?');
+const userFirstNumber = +prompt(`${userName}! Write the first number: `);
+const userSecondNumber = +prompt(`Write the second number: `);
+
+const userOperation = prompt(`${userName} Select an option : 'add', 'sub', 'mult', 'div'`);
+
+if (userOperation === 'add') {
+    alert(`${userFirstNumber} + ${userSecondNumber} = ${userFirstNumber + userSecondNumber }`)
+} else if (userOperation === 'sub') {
+    alert (`${userFirstNumber} - ${userSecondNumber} = ${userFirstNumber - userSecondNumber }`)
+} else if (userOperation === 'mult') {
+    alert(`${userFirstNumber} * ${userSecondNumber} = ${userFirstNumber * userSecondNumber }`)
+} else if (userOperation === 'div') {
+    alert(`${userFirstNumber} / ${userSecondNumber} = ${userFirstNumber / userSecondNumber }`)
+}
