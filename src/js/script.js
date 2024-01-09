8c6c830ba9c64dde1c337d0f41d04a66dbb3ab9f
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-09 20:11:57 +0100

    hw_11 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const numOrStr = prompt('input number or string');
+
+switch (true) {
+    case numOrStr === null:
+        alert('ви скасували');
+        break;
+    case  numOrStr.trim() === '':
+        alert('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        alert(' number is Ba_NaN');
+        break;
+    default:
+        alert('OK!');
+}
