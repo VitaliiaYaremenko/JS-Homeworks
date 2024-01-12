08b8c51e3a115c23b3d143dbc75af1d5fe4e30f2
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-12 15:38:10 +0100

    hw_20 added

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,54 @@
 'use strict';
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const lengthCharacters = characters.length;
+let result = '';
+
+function generateKey (length, characters) {
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * lengthCharacters);
+        result += characters[randomIndex];
+    }
+    return result;
+}
+const randomString = generateKey(16, characters);
+console.log(randomString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let newString = '';
+// let counter = 0;
+// function generateKey(length, characters) {
+//
+//     while (counter < length) {
+//         newString += characters.charAt(Math.floor(Math.random() * lengthCharacters));
+//         counter += 1;
+//     }
+//     return newString;
+// }
+// const result = generateKey(16, characters);
+// console.log(result);
