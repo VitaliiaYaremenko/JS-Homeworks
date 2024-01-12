79e17933d008a3da69736ccd6fb00ddb97d1fec9
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-12 21:06:20 +0100

    hw_21 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const liters = 48;
+
+function calcPotato (value = 13, quantity = 4) {
+    const potato = liters * quantity;
+
+    const weight = Math.ceil(potato * (75 / 1000));
+
+    const cost = weight * value;
+
+    return alert(`For 48 liters of borscht you need: ${weight}kg potatoes! \nCost of potatoes: ${cost}`);
+}
+ calcPotato();
