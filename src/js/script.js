4cecfd704e99e63f377a17e87ba6162c56925412
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-14 16:39:30 +0100

    hw_19 fixed check

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,11 @@ const userNumber = +prompt('Write your number : ');
 const degree = +prompt('Write the degree for your number: ');
 
 function calcDegreeConversion (userNumber, degree = 1) {
-    if (isNaN(userNumber) && isNaN(degree)) {
-        return Error ;
-    } else {
-        return userNumber**degree;
-        // Math.pow(userNumber, degree);
-    }
+
+    if (typeof userNumber !== 'number' && userNumber !== Number.isNaN(userNumber)) return Error ;
+    if (typeof degree !== 'number' && degree !== Number.isNaN(degree)) return Error ;
+
+    return userNumber**degree;
 }
 const result = calcDegreeConversion(userNumber, degree);
 alert(`Number in degree: ${result}`);
