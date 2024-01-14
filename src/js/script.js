5602f4391d83f32fe51ffe3b6fedbfb88f1061be
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-14 23:22:24 +0100

    hw_23 fixed check

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+const positiveNumber = function (arr) {
+    const exampleArr = [];
+
+    if (Array.isArray(arr) && arr.length === 0) return 'Пустий обєкт';
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0 ) break;
+        exampleArr.push(arr[i]);
+    }
+    if (Array.isArray(exampleArr) && exampleArr.length === 0) return null;
+    return exampleArr;
+}
+console.log(positiveNumber(arr));
+console.log(arr);
+
+
