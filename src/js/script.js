2a254128a80f740e2b27cc1805303467d2f46edf
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-16 14:21:37 +0100

    hw_25 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const getRandomNum = function () {
+
+    return Math.floor(Math.random() * 101);
+}
+
+const arrNum = [];
+
+for (let i = 0; arrNum.length < 100; i++) {
+
+    const randomNumber = getRandomNum();
+
+    if(!arrNum.includes(randomNumber)) {
+        arrNum.push(arrNum[randomNumber]);
+    }
+    console.log(randomNumber);
+}
+
+console.log(`Yor random number: ${getRandomNum()}`)
+
+
+
+
+
