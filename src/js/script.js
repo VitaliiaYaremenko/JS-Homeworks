0979823e529ab5080d300b8d19c6fee37f6fad83
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-19 14:22:28 +0100

    hw_24 fixed

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,10 @@
 
 const userAge = +prompt('Напишіть скільки вам років: ');
 
-if (typeof userAge !== 'number' && userAge !== Number.isNaN(userAge)) alert (`Ви ввели не число! `);
+if (typeof userAge !== 'number' &&  Number.isNaN(userAge)) alert (`Ви ввели не число! `);
 
 const year1 = [1, 21, 31, 41, 51, 61, 71, 81, 91, 101, 121];
   for (let i = 0; i < year1.length; i++) {
       if (year1[i] === userAge) alert(`Тобі ${userAge} - рік!`);
   }
     alert(`Тобі ${userAge} - років`);
-
