43efcadb9a6519cd134901142ce25e56042c5f6d
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-19 14:48:04 +0100

    hw_22 added check

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@
 function padString (string, length, symbol = '*', sideRight = true) {
 
   if (typeof string !== 'string') throw new Error('Passed value is not a string');
-  if (typeof length !== 'number' &&  Number.isNaN(length)) throw new Error(`The transferred value is not a number`);
+  if (typeof length !== 'number' ||  Number.isNaN(length)) throw new Error(`The transferred value is not a number`);
 
   if (string.length === length) return string;
   if (string.length > length) return string.slice(0, length);
