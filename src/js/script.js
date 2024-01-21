39a04f12be137ba55b8f842a0028048c1aa67acd
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-21 12:59:56 +0100

    hw_25 added changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,24 @@
 'use strict';
 
-const getRandomNum = function () {
-const arr = [];
-
-while (arr.length < 100 ) {
-
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    if (arr.indexOf(randomNumber) === -1) arr.push(randomNumber);
-  }
- return arr;
-}
-console.log(`Yor random number: ${getRandomNum()}`)
-
-// ________________________________________
-
+const array = [];
 const getNumberRm = function () {
-    const array = [];
 
-    for (let i = 0;   array.length < 100; i++) {
-        const numRn = Math.floor(Math.random() * 100) + 1;
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-        if (!array.includes(numRn)) {
-            array.push(numRn);
-        }
-    } return array;
-
+    if (!array.includes(randomNumber)) {
+        array.push(randomNumber);
+        return randomNumber;
+    }
+    while (array.includes(randomNumber)) {
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+    }
+    array.push(randomNumber);
+    return randomNumber;
 }
-console.log(getNumberRm());
+for (let i = 0; i < 100; i++) {
+    console.log(getNumberRm());
+}
+
 
 
 
