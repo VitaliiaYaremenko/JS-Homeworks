b339999068e4ed0e541f2a4034ab8e68857c82d1
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-22 19:05:48 +0100

    hw_26 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
-'use strict';
+const greet = function () {
+  return `Hello! Welcome ${this.name}!`;
+};
+
+const userAge = function () {
+  return `Your age ${this.name} - ${this.age}!`;
+};
+
+const createUser = function (name, age) {
+  const user = {
+    name,
+    age,
+  };
+  user.greet = greet;
+  user.userAge = userAge;
+  return user;
+};
+
+const firstUser = createUser('Anna', 19);
+alert(`${firstUser.greet()}\n${firstUser.userAge()}`);
+
+const secondUser = createUser('Sem', 25);
+alert(`${secondUser.greet()}\n${secondUser.userAge()}`);
+
+const thirdUser = createUser('Nik', 50);
+alert(`${thirdUser.greet()}\n${thirdUser.userAge()}`);
