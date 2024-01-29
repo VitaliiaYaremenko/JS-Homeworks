85ae4744228a5c5347de6e27f9dda4d0acbeb53e
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-29 22:14:02 +0100

    completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-'use strict';
+const updateQueryString = function (url, key, value) {
+  const urlAddress = new URL(url);
+
+  const paramsUrl = urlAddress.searchParams;
+  urlAddress.searchParams.append(key, value);
+  urlAddress.search = paramsUrl.toString();
+
+  return urlAddress.toString();
+};
+
+console.log(updateQueryString('https://example.com/path?query=123', 'query', '266'));
