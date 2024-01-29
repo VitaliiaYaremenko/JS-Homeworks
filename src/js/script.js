3427a8b224fe4a00ffbfa5207120921192cdae1f
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-29 20:43:44 +0100

    completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,34 @@
-'use strict';
+const parseUrl = function (url) {
+  const urlAddress = new URL(url);
+
+  return {
+    href: urlAddress.href,
+    protocol: urlAddress.protocol,
+    host: urlAddress.host,
+    hostname: urlAddress.hostname,
+    port: urlAddress.port,
+    pathname: urlAddress.pathname,
+    search: urlAddress.search,
+    hash: urlAddress.hash,
+    origin: urlAddress.origin,
+  };
+};
+
+const result = parseUrl('https://example.com:8080/path/name?query=123#hash');
+console.log(result);
+
+const userUrl = function () {
+  return {
+    href: window.location.href,
+    protocol: window.location.protocol,
+    host: window.location.host,
+    hostname: window.location.hostname,
+    port: window.location.port,
+    pathname: window.location.pathname,
+    search: window.location.search,
+    hash: window.location.hash,
+    origin: window.location.origin,
+  };
+};
+
+console.log(userUrl());
