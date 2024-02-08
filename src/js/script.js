72e7c9f1f110d146f902cc995f531a6e7953a993
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-02-08 18:59:57 +0100

    completed hw_33

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,40 @@
-'use strict';
+const runCallbacks = function (callbacks, value) {
+  let result = value;
+
+  for (let i = 0; i < callbacks.length; i += 1) {
+    result = callbacks[i](result);
+  }
+  return result;
+};
+
+const addFirst = (x) => x + 1;
+const square = (x) => x * x;
+
+const callbacks = [addFirst, square, addFirst];
+
+const firstResult = runCallbacks(callbacks, 1);
+console.log(firstResult);
+// ____________________________________________________
+
+const composeFunctions = function (...functions) {
+  return function (value) {
+    return functions.reduceRight((currentValue, currentFunc) => currentFunc(currentValue), value);
+  };
+};
+
+const addOne = function (x) {
+  return x + 1;
+};
+
+const secondSquare = function (x) {
+  return x * x;
+};
+
+const multiplyByTwo = function (x) {
+  return x * 2;
+};
+
+const composedFunction = composeFunctions(addOne, secondSquare, multiplyByTwo);
+
+const result = composedFunction(3);
+console.log(result);
