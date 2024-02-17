adf597af51cf5b526579b7bd5e3639464a1fa7e6
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-02-17 16:00:45 +0100

    completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-'use strict';
+const myApply = function (func, context, argsArray) {
+  const result = func(...argsArray);
+
+  context = context || window;
+
+  return result;
+};
+
+const userData = {
+  name: 'Alex',
+};
+
+const salutation = function (greeting) {
+  return console.log(`${greeting} ${this.name}`);
+};
+myApply(salutation, userData, ['Hello !']);
