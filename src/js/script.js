c29b3cdac2a9619af5cd59345cc0e747920909dd
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-02-18 12:44:32 +0100

    added changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
+const myFlat = function (arr) {
+  if (arguments.length > 1 || !Array.isArray(arr)) {
+    throw new Error('throw new Error(\'Function accepts only 1 argument, too much arguments provided\');');
+  }
+
+  const flatten = function (inputArray) {
+    let flatArray = [];
+
+    for (const item of inputArray) {
+      if (Array.isArray(item)) {
+        flatArray = flatArray.concat(flatten(item));
+      } else {
+        flatArray.push(item);
+      }
+    }
+    return flatArray;
+  };
+  return flatten(arr.slice());
+};
+
 const arrOn = [1, 2, 3, 1, 2, 3, 4, 2, 3, 4];
 
 const complexArray = [
@@ -6,28 +26,7 @@ const complexArray = [
   [[10, 11], 12, 13],
 ];
 
-const myFlat = function (...arr) {
-  if (arr.length !== 1) {
-    throw new Error('throw new Error(\'Function accepts only 1 argument, too much arguments provided\');');
-  }
-
-  if (!Array.isArray(arr)) {
-    throw new Error('You did not pass an array!');
-  }
-};
-
-const flattenedArray = [];
-
-const flattener = function (array) {
-  for (let i = 0; i < array.length; i += 1) {
-    if (Array.isArray(array[i])) {
-      flattener(array[i]);
-    } else {
-      flattenedArray.push(array[i]);
-    }
-    return flattenedArray;
-  }
-  return flattener(arr);
-};
-
-console.log(myFlat(complexArray));
+const flattenArray = myFlat(complexArray);
+const resultArray = myFlat(arrOn);
+console.log(flattenArray);
+console.log(resultArray);
