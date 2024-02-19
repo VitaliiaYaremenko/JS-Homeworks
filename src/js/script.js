c060c5a2724f53bb57a9c59bc53279812bf0139e
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-02-19 21:49:53 +0100

    added object

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-'use strict';
+const productCatalog = {
+  firstProduct: {
+    name: 'grill',
+    price: 10000,
+  },
+  secondProduct: {
+    name: 'telephone',
+    price: 30000,
+  },
+  thirdProduct: {
+    name: 'glasses',
+    price: 4000,
+  },
+  fourthProduct: {
+    name: 'laptop',
+    price: 50000,
+  },
+  fifthProduct: {
+    name: 'cactus',
+    price: 500,
+  },
+};
+console.log(productCatalog);
