f8b669192f4ac42f0f6cbe1d90598f5a5e50402a
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-02-19 22:22:24 +0100

    completed hw_42

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-'use strict';
+const string1 = '{"name": "John", "age": 30}';
+const string2 = '{"name": "John", "age":';
+
+const isValidJSON = (str) => JSON.parse(str);
+
+try {
+  console.log(isValidJSON(string1));
+  console.log(isValidJSON(string2));
+} catch {
+  console.log('"Invalid JSON string"');
+  console.dir(Error);
+}
