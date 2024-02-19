9a9c9e3d2d2fcc984c99e2564bf2a65a266935c1
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-02-19 21:19:09 +0100

    completed hw_40

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
-'use strict';
+const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter((word) => word.length > 5);
+console.log(result);
+
+// _______________________________________
+const numbers = [1, 12, 4, 8, 10, 11];
+
+const pairedResult = numbers.filter((num) => num % 2 === 0);
+const sumResult = pairedResult.reduce((sum, number) => sum + number, 0);
+
+console.log(pairedResult);
+console.log(sumResult);
+
+// _______________________________________
+
+const arrayNum = [2, 14, 6, 8, 12];
+const sum = arrayNum.reduce((acc, number) => number + acc, 0);
+const average = Math.round(sum / arrayNum.length);
+
+console.log(sum);
+console.log(average);
