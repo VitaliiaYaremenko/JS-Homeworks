dd0046f99b4534f7d75035572deffac92d97afa8
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-02-20 20:27:45 +0100

    added changes hw_41

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,32 @@
-const productCatalog = {
-  firstProduct: {
-    name: 'grill',
-    price: 10000,
-  },
-  secondProduct: {
-    name: 'telephone',
-    price: 30000,
-  },
-  thirdProduct: {
-    name: 'glasses',
-    price: 4000,
-  },
-  fourthProduct: {
-    name: 'laptop',
-    price: 50000,
-  },
-  fifthProduct: {
-    name: 'cactus',
-    price: 500,
-  },
+const productCatalog = [
+  { name: 'grill', price: 10000 },
+  { name: 'telephone', price: 30000 },
+  { name: 'glasses', price: 4000 },
+  { name: 'laptop', price: 50000 },
+  { name: 'cactus', price: 500 },
+];
+
+const updatedPrices = (object) => {
+  const discount = object.map((obj) => ({ ...obj, price: obj.price * 0.9 }));
+
+  return discount;
 };
-console.log(productCatalog);
+const newPrice = updatedPrices(productCatalog);
+console.log(newPrice);
+// ____________________________________________
+
+const students = [
+  { name: 'Viktor', grade: 100 },
+  { name: 'Kira', grade: 65 },
+  { name: 'Nikita', grade: 54 },
+  { name: 'Alex', grade: 95 },
+];
+
+const getEstimate = (array) => {
+  const result = array.filter((object) => object.grade > 90);
+
+  return result;
+};
+
+const newStudentsGrade = getEstimate(students);
+console.log(newStudentsGrade);
