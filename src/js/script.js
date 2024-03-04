42372f66a9b44910c548b56adfd2751f6cef5509
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-03-04 23:15:31 +0100

    added changes hw_43

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-'use strict';
+(function () {
+  const generateList = (array) => {
+    const ul = document.createElement('ul');
+
+    array.forEach((item) => {
+      const li = document.createElement('li');
+      let childElement;
+
+      if (Array.isArray(item)) {
+        childElement = (generateList(item));
+      } else {
+        childElement = (document.createTextNode(item));
+      }
+      li.appendChild(childElement);
+      ul.appendChild(li);
+    });
+    return ul;
+  };
+  document.body.appendChild(generateList([1, 2, 3]));
+  document.body.appendChild(generateList([1, 2, [1.1, 1.2, ['1.2.1', '1.2.2'], 1.3], 3]));
+}());
