4931b1eccf872e7e9e5d45da112a07debe7fa162
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-03-05 17:41:11 +0100

    completed hw_44

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
-'use strict';
+(function () {
+  function createTable() {
+    const table = document.createElement('table');
+
+    let addiNumbers = 1;
+
+    for (let i = 0; i < 10; i += 1) {
+      const newRow = table.insertRow(i);
+
+      for (let j = 0; j < 10; j += 1) {
+        const newCell = newRow.insertCell(j);
+        newCell.textContent = addiNumbers++;
+      }
+    }
+
+    const oldTable = document.querySelector('table');
+    if (oldTable) {
+      oldTable.remove();
+    }
+
+    document.body.appendChild(table);
+  }
+  createTable();
+}());
