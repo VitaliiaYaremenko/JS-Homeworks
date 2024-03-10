f374a8a0ce958aeb6c861e2641d610d7b6e618df
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-03-10 18:53:55 +0100

    completed hw_45

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-'use strict';
+(function () {
+  const checkNumber = () => {
+    const inputNumber = document.getElementById('numberEnter').value;
+    const result = (inputNumber % 2 === 0) ? 'Число парне' : 'Число не парне';
+
+    alert(result);
+  };
+
+  document.getElementById('checkBtn').addEventListener('click', checkNumber);
+}());
